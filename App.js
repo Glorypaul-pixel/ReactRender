82880ca4a79f4eae1a0778a506e77a718482045a
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-15 23:18:02 +0100

    lets learn something new

---
+++ App.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li> Home</li>
+          <li> Service</li>
+          <li> About</li>
+          <li> Blog</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
